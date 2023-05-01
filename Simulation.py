e13d34a8c2d462a78141792f066dee7ce2b4336c
StormFire <73555407+TheElementCraft@users.noreply.github.com>
2023-05-01 18:46:42 +0200

    Verzweiflunbg

--- Simulation.py
+++ Simulation.py
@@ -1,8 +1,10 @@
 from math import *
+import plotly.express as px
+import pandas as pd
 
 class rocket():
     masse = 2
-    runtime = 20
+    runtime = 60
     tw_f = 34
     hoehe = 0
     entfernung = 0
@@ -12,32 +14,68 @@ class rocket():
     ve = 0
     ae = 0
 
+    flugzeit = 0
+
 class Umgebung():
     g = 9.81
 
 u = Umgebung()
 r = rocket()
-
+T = []
 H = []
 E = []
+VH = []
+AH = []
 
-t = 1000 # Simulationszeit
+k = 0.001 # Luftwiederstand
 
-si = 1 #SimulationsIntervall
+si = float(input("Genauigkeit (Kleiner ist genauer):  ")) #SimulationsIntervall
+
+t = int(1000/si) # MaxSimulationsschritte
+
+sz = 0
 
 def TriebwerkLaufzeit(runtime,time, tw_f):
     if time >= runtime:
         return(0)
     else:
         return(tw_f)
+def MaxHöhe(Höhe):
+    maxh = 0
+    for h in Höhe:
+        if h > maxh:
+            maxh = h
+    return(maxh)
 
 for timer in range(1,t+1):
-    r.ah = (r.tw_f-r.masse*u.g)/r.masse
-    r.vh = r.vh + r.ah
-    r.hoehe = r.hoehe+r.vh
+    r.hoehe = r.hoehe+r.vh * si
+    r.vh = r.vh + r.ah * si
+    r.tw_f = TriebwerkLaufzeit(r.runtime, sz, r.tw_f)
+    VH.append(r.vh)
+    if r.ah != 0:
+        r.ah = float(((r.tw_f-r.masse*u.g)/r.masse))-k*r.vh*r.vh*(r.vh/sqrt(r.vh*r.vh))
+    else:
+        r.ah = float(((r.tw_f-r.masse*u.g)/r.masse))
+    AH.append(r.ah)
     H.append(r.hoehe)
-    r.tw_f = TriebwerkLaufzeit(r.runtime, timer, r.tw_f)
-    if r.hoehe<=0:
-        print(H)
-        print("Aufgekommen nach "+str(timer)+" sek")
+    T.append(timer)
+    
+    if r.hoehe<0:
+        print("Aufgekommen nach "+str(sz)+" sek Maximal Höhe: "+str(MaxHöhe(H))+" Schritte: "+str(timer))
+        
         break
+    sz+=si
+print("Finished")
+
+TH = pd.DataFrame({'x': T,
+                   'y': H,})
+TVH =pd.DataFrame({'x': T,
+                   'y': VH,})
+TAH =pd.DataFrame({'x': T,
+                   'y': AH,})
+fig1 = px.line(TH, x = 'x', y = 'y', markers = True)
+fig2 = px.line(TVH, x = 'x', y = 'y', markers = True)
+fig3 = px.line(TAH, x = 'x', y = 'y', markers = True)
+fig1.show()
+fig2.show()
+fig3.show()
