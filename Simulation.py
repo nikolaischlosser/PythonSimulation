c1a5c0c78a5bd1d3b2c0ecaa056f2ae44c5e4579
PFornax <69404920+PFornax@users.noreply.github.com>
2023-05-01 16:32:28 +0200

    raketen klasse

--- Simulation.py
+++ Simulation.py
@@ -1,9 +1,28 @@
 from math import *
-print("hallo");
-Rakete = [2,5,25] # Masse in kg, Triebwerk Laufzeeit in s, Kraft von triebwerk in n
 
-h = 0 #Höhe
 
-e = 0 #Entfernung
+# Rakete = [2,5,25] # Masse in kg, Triebwerk Laufzeeit in s, Kraft von triebwerk in n
+
+
+
+# h = 0 #Höhe
+
+# e = 0 #Entfernung
 
 t = 10 # Simulationszeit
+
+class rocket():
+    masse = 2
+    runtime = 5
+    tw_f = 25
+    hoehe = 0
+    entfernung = 0
+
+
+r = rocket()
+
+
+
+
+
+
